cd6b7731976f0006f6fb3a780b892700f85d0ef1
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-09-15 13:56:49 +0900

    Home screen 구현

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,6 +1,59 @@
+import { useQuery } from "react-query";
+import { IGetMoviesResult, getMovies } from "../api";
+import styled from "styled-components";
+import { makeImagePath } from "../utils";
+
+const Wrapper = styled.div`
+  background: black;
+`;
+
+const Loader = styled.div`
+  height: 200vh;
+  display: flex;
+  justify-content: center;
+  align-itmes: center;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+  font-size: 30px;
+  width: 50%;
+`;
+
 function Home() {
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
+    ["movies", "nowPlaying"],
+    getMovies
+  );
+  console.log(data, isLoading);
   return (
-    <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>home</div>
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner bgPhoto={makeImagePath(data?.results[9].backdrop_path || "")}>
+            <Title>{data?.results[9].title}</Title>
+            <Overview>{data?.results[9].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
   );
 }
 export default Home;
